7af74448dda28c176329ee6943fcb47e50b38c77
unknown <timothytran051@gmail.com>
2025-03-06 14:25:05 -0800

    password hashing and verification works, working on relationship between User and Task models

--- auth.py
+++ auth.py
@@ -5,8 +5,13 @@ def hash_password(plain_password:str ) -> str:
     return bcrypt.hashpw(plain_password.encode('utf-8'), salt).decode('utf-8')
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return bcrypt.checkpw(plain_password.encode('utf-8'), hash_password.encode('utf-8'))
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
-hashed_pw = hash_password("password123")
-print (hashed_pw)
-verify = verify_password("password123","$2b$12$EI16n/wsP0QDvUnexBHl3On692MFy2ErU5b0H8iaXf3bhHYGY9ewm")
+#test hashing
+# hashed_pw = hash_password("password123")
+# print (hashed_pw)
+
+#test verify
+# stored_hash = hash_password("password123")
+# print(verify_password("password123", stored_hash))
+# print(verify_password("wrongpass", stored_hash))
