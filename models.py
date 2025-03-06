e613e4893bd22ef503c144d12c9a2764b8d19d54
unknown <timothytran051@gmail.com>
2025-03-06 14:20:10 -0800

    user table set up, working on hasing password with bcrypt for future authentication

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
-
 Base = declarative_base()
 
 # CREATE TABLE Task(
@@ -13,9 +12,16 @@ Base = declarative_base()
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key = True, index = True, autoincrement = True)
-    user_id = Column(Integer, ForiegnKey("users.id"), nullable = False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable = False)
     title = Column(String(255), nullable = False, index = True)
     description = Column(String(255), nullable = True)
     Completed = Column(Boolean, default = False, nullable = False)
     
     user = relationship("User", back_populates = "tasks")
+
+class User(Base):
+    id = Column(Integer, primary_key = True, index = True, autoincrement = True)
+    username = Column(String(50), unique = True, index = True, nullable = False)
+    email = Column(String(255), unique = True, index = True, nullable = False)
+    hashed_password = Column(String(255), nullable = False)
+    relationship("Task")
