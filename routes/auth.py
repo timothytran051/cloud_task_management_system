823094e9c0420b61084a0d00440626d7d95577a8
timothytran051 <timothytran051@gmail.com>
2025-03-09 23:24:18 -0700

    login_user function verification complete, need to learn JWT and token generation

--- routes/auth.py
+++ routes/auth.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from models import User
 from database import get_db
-from utils.auth import hash_password
+from utils.auth import hash_password, verify_password
 from pydantic import BaseModel, EmailStr
-from schemas.schemas import UserCreate
+from schemas.schemas import UserCreate, UserLogin
 
 router = APIRouter()
 
@@ -13,7 +13,7 @@ router = APIRouter()
 async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     existing_user = await db.execute(select(User).where((User.username == user.username) | (User.email == user.email)))
     # existing_email = await db.execute(select(User).where(User.email == user.email)) # SELECT * FROM users WHERE email = {user.email}
-    if existing_user.scalars().first() or existing_user.scalars().first():
+    if existing_user.scalars().first():
         raise HTTPException(status_code=400, detail="Username or Email already registered")
     
     secure = hash_password(user.password)
@@ -22,3 +22,16 @@ async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     await db.commit()
     
     return {"message": "User Registered"}
+
+@router.post("/login")
+async def login_user(user: UserLogin, db: AsyncSession = Depends(get_db)):
+    login = await db.execute(select(User).where((User.username == user.username) | (User.email == user.email)))
+    user_data = login.scalars().first()
+    if not user_data:
+        raise HTTPException(status_code = 400, detail = "Username or Email not found")
+    verify = verify_password(user.password, user_data.hashed_password)
+    
+    if verify == False:
+        raise HTTPException(status_code = 400, detail = "Incorrect Password")
+    
+    
--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
+    password: str
+    
+class UserLogin(BaseModel):
+    Username: Optional[str]
+    email: Optional[str]
     password: str
